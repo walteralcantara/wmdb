3a943f661be8bfd3c2dd1f2ec9c07b9517d3d2d2
Walter Alcantara <waltermalcantara@gmail.com>
2021-05-01 23:57:52 -0300

    Adjusted carousel home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
+import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
 import Carousel from 'react-bootstrap/Carousel';
 
 
@@ -12,60 +13,40 @@ import { formatGenre } from '../utils/formatGenre';
 import styles from './Home.module.scss';
 
 
-export default function Home({trendingMoviesList}) {
+export default function Home({trendingMoviesList, latestMovies}) {
 
-  console.log(trendingMoviesList);
+  // console.log(trendingMoviesList);
 
   const [index, setIndex] = useState(0);
-
+  
   const handleSelect = (selectedIndex, e) => {
     setIndex(selectedIndex);
   };
 
   return (
+    
     <div className={styles.homePage}>
-
-      <section>
+      <section className={styles.carousel}>
         <Carousel activeIndex={index} onSelect={handleSelect}>
-        <Carousel.Item>
-          <img
-            className="d-block w-200"
-            src={trendingMoviesList[0].backdrop}
-            alt="First slide"
-          />
-          <Carousel.Caption>
-            <h3>{trendingMoviesList[0].title}</h3>
-            <p>{trendingMoviesList[0].description}</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={trendingMoviesList[1].backdrop}
-            alt="Second slide"
-          />
+          { latestMovies.map((slideMovie, index) => {
+            // console.log('SLIDE MOVIE:',slideMovie, index);
 
-          <Carousel.Caption>
-            <h3>{trendingMoviesList[1].title}</h3>
-            <p>{trendingMoviesList[1].description}</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={trendingMoviesList[2].backdrop}
-            alt="Third slide"
-          />
+            return (
+              <Carousel.Item key={slideMovie.id}>
+                <img
+                  src={slideMovie.backdrop}
+                  alt={slideMovie.title}
+                />
 
-          <Carousel.Caption>
-            <h3>{trendingMoviesList[2].title}</h3>
-            <p>
-            {trendingMoviesList[2].description}
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
-    </section>
+                <Carousel.Caption>
+                  <h3>{slideMovie.title}</h3>
+                  <p>{slideMovie.description}</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+            )
+          }) }
+        </Carousel>
+      </section>
 
       <section className={styles.movies}>
         <h2>Filmes da semana</h2>
@@ -103,12 +84,10 @@ export default function Home({trendingMoviesList}) {
         </div>
 
       </section>
-
     </div>
   );
 
 }
-
 
 export const getServerSideProps = async () => {
 
@@ -119,11 +98,12 @@ export const getServerSideProps = async () => {
     }
   })
 
-  console.log(response.data.results);
+  // console.log(response.data.results);
 
   const trendingMoviesList = response.data.results.map(movie => {
-    const posterURL = 'https://image.tmdb.org/t/p/w200';
-    const backdropURL = 'https://image.tmdb.org/t/p/original'
+    
+  const posterURL = 'https://image.tmdb.org/t/p/w200';
+  const backdropURL = 'https://image.tmdb.org/t/p/original'
 
     return {
       id: movie.id,
@@ -137,9 +117,12 @@ export const getServerSideProps = async () => {
     }
   })
 
+  const latestMovies = trendingMoviesList.slice(0, 6);
+
   return {
     props: {
       trendingMoviesList,
+      latestMovies,
     }
   }
 
