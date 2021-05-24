43fc0927f4c9349167edab3e3267bf1ede092765
Walter Alcantara <waltermalcantara@gmail.com>
2021-05-23 21:28:45 -0300

    Implemented animation with framer motion

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -4,6 +4,8 @@ import ReactPlayer from 'react-player/youtube'
 import { useContext } from 'react';
 import { ContextAPI } from '../../context/ContextAPI';
 
+import { motion } from 'framer-motion'
+
 
 import styles from './Modal.module.scss'
 
@@ -11,14 +13,26 @@ export default function Modal(movieInfo, ...rest) {
 
   const { isModalOpen, setIsModalOpen } = useContext(ContextAPI);
 
-    // console.log('movie video component:', movieVideo)
-    console.log('movie info component:', movieInfo, ...rest)
+  const variants = {
+    open: {opacity: 1, y: 0},
+    closed: {opacity: 0, y: "-25%" },
+
+    
+  }
 
   return (
-    <div className={isModalOpen ? `${styles.modalContainer} ${styles.active}` : `${styles.modalContainer}`}>
+    <>
+    <div className={isModalOpen ? `${styles.modalOverlay} ${styles.active}` : ''}></div>
+    
+    <motion.div 
+      className={isModalOpen ? `${styles.modalContainer} ${styles.active}` : `${styles.modalContainer}`}
+      animate={isModalOpen ? "open" : "closed"}
+      variants={variants}
+      >
+
         <header className={styles.modal__header}>
             <h1 className={styles.modal__title}>{movieInfo.element.title}</h1>
-            <i className={styles.modal__close} onClick={() => setIsModalOpen(false)}>
+            <i className={styles.modal__close} onClick={() => setIsModalOpen(!isModalOpen)}>
               <svg viewBox="0 0 357 357">
                 <polygon points="357,35.7 321.3,0 178.5,142.8 35.7,0 0,35.7 142.8,178.5 0,321.3 35.7,357 178.5,214.2 321.3,357 357,321.3 214.2,178.5"/>
               </svg>
@@ -27,10 +41,9 @@ export default function Modal(movieInfo, ...rest) {
         <main className={styles.modal__videoContainer}>
             <ReactPlayer 
                 url={`https://www.youtube.com/watch?v=${movieInfo.video.results[0].key}`}
-                height={450}
             />
         </main>
-    </div>
-    
+    </motion.div>
+    </>
   );
 }
